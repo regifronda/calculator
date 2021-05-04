8b7ea282f39da3eb7a467c27ab7ca422f88e15b1
Regi Fronda <regisfronda@gmail.com>
2021-05-04 03:05:10 -0700

    Add functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,24 @@
+function add (num1, num2) {
+	console.log(num1 + num2);
+}
+
+function subtract (num1, num2) {
+	console.log (num1 - num2);
+}
+
+function multiply (num1, num2) {
+	console.log (num1 * num2);
+}
+
+function multiply (num1, num2) {
+	console.log (num1 * num2);
+}
+
+function divide (num1, num2) {
+	console.log (num1 / num2);
+}
+
+add(5, 2);
+subtract(5, 2);
+multiply(5, 2);
+divide(10, 2);
