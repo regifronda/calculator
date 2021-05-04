0786f1ce1918418e92cd67bd660950e1f49689f7
Regi Fronda <regisfronda@gmail.com>
2021-05-04 05:03:54 -0700

    Add operate function

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 function add (num1, num2) {
-	console.log(num1 + num2);
+	return num1 + num2;
 }
 
 function subtract (num1, num2) {
-	console.log (num1 - num2);
+	return num1 - num2;
 }
 
 function multiply (num1, num2) {
-	console.log (num1 * num2);
-}
-
-function multiply (num1, num2) {
-	console.log (num1 * num2);
+	return num1 * num2;
 }
 
 function divide (num1, num2) {
-	console.log (num1 / num2);
+	return num1 / num2;
 }
 
-add(5, 2);
-subtract(5, 2);
-multiply(5, 2);
-divide(10, 2);
+let operate = (num1, num2, operator) => {
+	if (operator == "add") {
+		add();
+	} else if (operator == "subtract") {
+		subtract();	
+	} else if (operator == "multiply") {
+		multiply();
+	}
+	else if (operator == "divide") {
+		divide();
+	}
+}
+
+console.log(add(5, 2));
+console.log(subtract(5, 2));
+console.log(multiply(5, 2));
+console.log(divide(10, 2));
