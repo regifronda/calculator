00f4003d1438f89aed5db1b1fb7d560f32198abd
Regi Fronda <regisfronda@gmail.com>
2021-05-06 01:49:25 -0700

    Add event listener to digits buttons

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function add (num1, num2) {
+function add(num1, num2) {
 	return num1 + num2;
 }
 
-function subtract (num1, num2) {
+function subtract(num1, num2) {
 	return num1 - num2;
 }
 
-function multiply (num1, num2) {
+function multiply(num1, num2) {
 	return num1 * num2;
 }
 
-function divide (num1, num2) {
+function divide(num1, num2) {
 	return num1 / num2;
 }
 
@@ -31,3 +31,17 @@ console.log(add(5, 2));
 console.log(subtract(5, 2));
 console.log(multiply(5, 2));
 console.log(divide(10, 2));
+
+const digits = document.querySelectorAll(".digits");
+
+digits.forEach(function showDigit(digit){
+	digit.addEventListener("click", function() {
+		console.log("click!");
+	});
+});
+/*only get console log for 1 digit button 
+const digits = document.querySelector(".digits");
+
+digit.addEventListener("click", () => {
+	console.log("click!")
+});*/
