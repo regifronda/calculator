dc8c57662e05f12624ff979b51922c17ebb37a8c
Regi Fronda <regisfronda@gmail.com>
2021-05-19 00:32:33 -0700

    removed all console.logs

--- script.js
+++ script.js
@@ -39,32 +39,23 @@ let operate = (num1, num2, operator) => {
 	}
 }
 
-console.log(add(5, 2));
-console.log(subtract(5, 2));
-console.log(multiply(5, 2));
-console.log(divide(10, 2));
-
 for (let i = 0; i < digits.length; i++) {
 	let digit = digits[i];
 	digit.addEventListener("click", () => {
 		storedNum += digit.textContent;
 		display.textContent = storedNum;
-		console.log("storedNum "+ storedNum + " is on display!");
 	});
 };
 
 operatorBtn.forEach((operator) => {
 	operator.addEventListener("click", function() {
 		if (firstNum && storedNum)  {
-			console.log("firstNum and storedNum already have numbers in them so showResult()!")
 			showResult();
 		}
 		
 	firstNum = storedNum;
-	console.log("storedNum " + storedNum + " is assigned to firstNum, which is now " + firstNum);
 
 	pressedOperator = operator.textContent;
-	console.log("pressedOperator is " + pressedOperator);
 	storedNum = "";
 	});
 });
@@ -72,7 +63,6 @@ operatorBtn.forEach((operator) => {
 equalsBtn.addEventListener("click", function() {
 	if ((storedNum === "" && firstNum === "" && pressedOperator === "/") || (storedNum === "0" && pressedOperator === "/")) {
 		alert("Don't divide by 0, silly!");
-		console.log("Don't divide by 0 silly!");
 		storedNum = "";
   		firstNum = "";
   		firstNumHolder = "";
@@ -85,14 +75,11 @@ equalsBtn.addEventListener("click", function() {
 
 	// prevents display box errors if pressing equals button too early
 	if ((storedNum && firstNum === "") || (storedNum === "" && firstNum === "")) {
-		console.log("pre-emptive equals occured!");
 		return;
 	}
 
 	// Trying to set display number to firstNum if pressedOperator and oldOperator are the same.
 	if (pressedOperator == oldOperator) {
-		console.log("operator hasn't changed!");
-		console.log("oldOperator: " + oldOperator + " pressedOperator " + pressedOperator);
 		firstNum = display.textContent;
     	storedNum = firstNumHolder;
 		showResult();
@@ -102,14 +89,11 @@ equalsBtn.addEventListener("click", function() {
     // Trying to set variable oldOperator to previous pressedOperator to check if operator has changed after clicking equals button
 	if (pressedOperator) {
 		oldOperator = pressedOperator;
-		console.log("oldOperator "+ oldOperator + " has been stored");
 	}
 	
 	// Allows user to press a number button, then an op button, then equals
 	storedNum = display.textContent;
 	// First number is kept while  storing the number on display
-	console.log("Clicked equals, but before showResult(), storedNum is set to display.textContent " + display.textContent);
-	console.log("Clicked equals and firstNum is " + firstNum);
 	showResult();
 });
 
@@ -125,7 +109,6 @@ clearBtn.addEventListener("click", function() {
 
 function showResult() {
 	let result = operate(parseFloat(firstNum), parseFloat(storedNum), pressedOperator)
-	console.log("result: " + result);
 	if (result.toString().length > 10) {
 		const round = (number, decimalPlaces) => {
 			const factorOfTen = Math.pow(10, decimalPlaces);
@@ -133,23 +116,15 @@ function showResult() {
 		}
 
 		let roundedResult = round(result, 10);
-		console.log("roundedResult: " + roundedResult);
 		display.textContent = roundedResult;
-		console.log("firstNum: " + firstNum + " storedNum: " + storedNum + " result on display is " + roundedResult);
     	// allows firstNum to persist in a following evaluation
 		firstNumHolder = storedNum;
-    	console.log("firstNumHolder " + firstNumHolder);
 		storedNum = roundedResult;
-		console.log("roundedResult " + roundedResult + " is assigned to storedNum, which is now " + storedNum);
 		return;
 	}
 
 	display.textContent = result;
-	console.log("display's length is " + display.textContent.length);
-	console.log("firstNum: " + firstNum + " storedNum: " + storedNum + " result on display is " + result);
     // allows firstNum to persist in a following evaluation
 	firstNumHolder = storedNum;
-    console.log("firstNumHolder " + firstNumHolder);
 	storedNum = result;
-	console.log("result " + result + " is assigned to storedNum, which is now " + storedNum);
 }
